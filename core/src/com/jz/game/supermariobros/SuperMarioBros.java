5b5d67f6a117fab141e07dd1d633b31efa6ee304
jzheng <jiangzheng1023@foxmail.com>
2023-06-13 23:14:55 +0800

    Initialize

---
+++ core/src/com/jz/game/supermariobros/SuperMarioBros.java
@@ -0,0 +1,26 @@
+package com.jz.game.supermariobros;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.jz.game.supermariobros.screens.PlayScreen;
+
+public class SuperMarioBros extends Game {
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        setScreen(new PlayScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
---
+++ core/src/com/jz/game/supermariobros/screens/PlayScreen.java
@@ -0,0 +1,55 @@
+package com.jz.game.supermariobros.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.jz.game.supermariobros.SuperMarioBros;
+
+public class PlayScreen implements Screen {
+    SuperMarioBros game;
+
+    Texture texture;
+
+    public PlayScreen(SuperMarioBros game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(1, 0, 0, 1);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
